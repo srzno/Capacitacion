613e4799bd53d7945d5f59fe4c54c4cffade0772
srzno <sorzanosantiago@gmail.com>
2023-03-25 14:02:54 -0600

    add update restaurante

--- server/controller/restauranteController.js
+++ server/controller/restauranteController.js
@@ -1,7 +1,18 @@
 import Restaurante from "../model/Restaurante.js"
+import mongoose from "mongoose"
 
-export const getRestaurante = (req, res) => {
-    res.send("<h1>restaurantes</h1>")
+export const getRestaurante = async (req, res) => {
+    try {
+        const restaurantes = await Restaurante.find() 
+    
+        res.status(201).send({
+            restaurantes
+        })
+    } catch (error) {
+        res.status(400).send({
+            message: error.message
+        })
+    } 
 }
 
 
@@ -23,4 +34,25 @@ export const crearRestaurante = async (req, res) => {
         })
     } 
 
+}
+
+export const updateRestaurante = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { nombre } = req.body
+
+        const restaurante = await Restaurante.findOne({ _id: new mongoose.Types.ObjectId(id) }) 
+        
+        restaurante.nombre = nombre
+
+        await restaurante.save()
+
+        res.status(200).send({
+            message: "Nombre del restaurante actualizado"
+        })
+    } catch (error) {
+        res.status(400).send({
+            message: error.message
+        })
+    } 
 }
--- server/rutas/restauranteRutas.js
+++ server/rutas/restauranteRutas.js
@@ -1,8 +1,9 @@
 import  express  from "express";
-import	{ getRestaurante, crearRestaurante } from "../controller/restauranteController.js"
+import	{ getRestaurante, crearRestaurante, updateRestaurante } from "../controller/restauranteController.js"
 const router = express.Router()
 
 router.get("/restaurante", getRestaurante)
 router.post("/restaurante", crearRestaurante)
+router.put("/restaurante/:id", updateRestaurante)
 
 export default router
